5006dbe18049dee9e4b91de32eb9e566a5b5fde8
TuLocalHost <54047744+elunacado@users.noreply.github.com>
2022-11-30 07:17:09 -0600

    Update Historial.h

--- Historial.h
+++ Historial.h
@@ -6,12 +6,14 @@ using namespace std;
 class Historial
 {
 private:
-
+	//propiedades de la clase Historial
 	string name;
 	string ID;
 	string title;
 
 public:
+	//metodos de la clase
+
 	inline Historial();
 	inline void setHistorial(string, string, string);
 	inline void EscribirHistorial();
