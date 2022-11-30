9e805468dd28749734ec30289f2e885fd89c6659
TuLocalHost <54047744+elunacado@users.noreply.github.com>
2022-11-30 07:19:47 -0600

    Update User.h

--- User.h
+++ User.h
@@ -1,3 +1,6 @@
+#ifndef User_HEADER
+#define User_HEADER
+
 #include <sstream>
 #include <iostream>
 using namespace std;
@@ -5,17 +8,20 @@ using namespace std;
 
 class User {
     private:
+        //propiedades de la clase User
         string username;
         string ID;
      
-
     public:
+        //metodos de la clase
         User();
         User(string, string);
         string giveUserInfo();
         string getUserName();
         string getID();
         
+        
+        
 };
 
-
+#endif
