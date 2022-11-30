78e5cda98ab9d7addf9a94ade8fd68c3a4294caf
TuLocalHost <54047744+elunacado@users.noreply.github.com>
2022-11-30 07:18:06 -0600

    Update Reader.cpp

--- Reader.cpp
+++ Reader.cpp
@@ -7,6 +7,7 @@ using namespace std;
 Reader::Reader() {};
 
 void Reader::ReadFiles() {
+//El reader se encarga de mostrar los datos del inventario
 
 	fstream file;
 	file.open("C:/Users/ethan/source/repos/Biblio/Biblio/inventario.txt", ios::in);
